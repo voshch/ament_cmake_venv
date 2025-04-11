793a19778d0345a375e961257eed81fce831432e
Vova Shcherbyna <dev@voshch.dev>
2025-04-11 17:32:52 +0200

    speedup symlinking

--- cmake/venv_install.cmake
+++ cmake/venv_install.cmake
@@ -1,6 +1,7 @@
 function(venv_install)
+    cmake_path(GET venv_install_dir PARENT_PATH target_dir)
     install(
-        DIRECTORY "${venv_dir}"
-        DESTINATION "${venv_install_dir}/.."
+        FILES "${venv_dir}"
+        DESTINATION "${target_dir}"
     )
 endfunction()
