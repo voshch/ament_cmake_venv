b8961c923c494124d442b0f52b3e5ae0b882c886
Vova Shcherbyna <dev@voshch.dev>
2025-04-18 15:26:32 +0200

    ensure clean pythonpath

--- cmake/venv_script.cmake
+++ cmake/venv_script.cmake
@@ -1,9 +1,20 @@
 function(venv_script script_name script_cmd)
+    set(options)
+    set(oneValueArgs PYTHONPATH)
+    set(multiValueArgs)
+    cmake_parse_arguments(PARSE_ARGV 0 arg
+        "${options}" "${oneValueArgs}" "${multiValueArgs}"
+    )
+
+    if(NOT DEFINED arg_PYTHONPATH)
+        set(arg_PYTHONPATH "")
+    endif()
+
     file(GENERATE
         OUTPUT "${CMAKE_CURRENT_BINARY_DIR}/scripts/${script_name}"
         CONTENT
             "#!/usr/bin/env sh
-${venv_install_python} ${script_cmd} \"$@\""
+PYTHONPATH='${arg_PYTHONPATH}' ${venv_install_python} ${script_cmd} \"$@\""
         FILE_PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE GROUP_READ GROUP_EXECUTE WORLD_READ WORLD_EXECUTE
     )
     install(
