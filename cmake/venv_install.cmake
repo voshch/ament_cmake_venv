8e8a887395f6b00bd932955fd81a420f70e88d5f
Vova Shcherbyna <dev@voshch.dev>
2025-09-14 21:21:01 +0200

    expose python executable

--- cmake/venv_install.cmake
+++ cmake/venv_install.cmake
@@ -1,7 +1,10 @@
+include("${ament_cmake_venv_DIR}/venv_script.cmake")
+
 function(venv_install)
     cmake_path(GET venv_install_dir PARENT_PATH target_dir)
     install(
         FILES "${venv_dir}"
         DESTINATION "${target_dir}"
     )
+    venv_script("python" "")
 endfunction()
